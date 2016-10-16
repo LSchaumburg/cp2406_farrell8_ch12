e3b31ecbf394bf1114f63920349018d421e6d91e
Luke <luke.schaumburg@my.jcu.edu.au>
2016-10-16 14:59:54 +1000

    Finished Debugging exercises.

---
+++ Exercises/ThisChapterDidntHaveAny.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Luke on 16/10/2016.
+ */
+public class ThisChapterDidntHaveAny {
+    public static void main(String[] args) {
+        System.out.println("this chapter was with 13 and 14. Chapter 14 is the one with the exercises");
+    }
+}
